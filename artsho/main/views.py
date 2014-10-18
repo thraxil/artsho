38dd5ececbe185fa46eb24ac41a287cda1e019d1
Anders Pearson <anders@columbia.edu>
2014-10-18 17:41:37 +0200

    flake8

--- artsho/main/views.py
+++ artsho/main/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
 from .models import Show
 
+
 class IndexView(TemplateView):
     template_name = "main/index.html"
 
