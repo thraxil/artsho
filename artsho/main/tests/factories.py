fa5b95268d04295202707948264951f876241f33
Anders Pearson <anders@columbia.edu>
2014-11-11 22:40:12 +0100

    factory supports both

--- artsho/main/tests/factories.py
+++ artsho/main/tests/factories.py
@@ -84,4 +84,5 @@ class AuctionItemFactory(factory.DjangoModelFactory):
 class BidFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Bid
     auctionitem = factory.SubFactory(AuctionItemFactory)
+    item = factory.SubFactory(ItemFactory)
     user = factory.SubFactory(UserFactory)
