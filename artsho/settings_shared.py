3f91efcde2f28a5b9720681dd8f8077cca4b04ad
Anders Pearson <anders@columbia.edu>
2016-01-26 22:53:39 +0000

    make MIDDLEWARE_CLASSES a list instead of tuple

--- artsho/settings_shared.py
+++ artsho/settings_shared.py
@@ -71,7 +71,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
     'django_statsd.middleware.GraphiteRequestTimingMiddleware',
     'django_statsd.middleware.GraphiteMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -81,7 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'impersonate.middleware.ImpersonateMiddleware',
     'waffle.middleware.WaffleMiddleware',
-)
+]
 
 ROOT_URLCONF = 'artsho.urls'
 
