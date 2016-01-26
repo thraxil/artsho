5cf1a2dfa3a5bbfb3ca0646b23a42853e290ca21
Anders Pearson <anders@columbia.edu>
2016-01-26 21:59:16 +0000

    opbeat integration

--- fabfile.py
+++ fabfile.py
@@ -13,6 +13,16 @@ def prepare_deploy():
     local("make")
 
 
+def opbeat():
+    url = ("https://intake.opbeat.com/api/v1/organizations/" +
+           "68fbae23422f4aa98cb810535e54c5f1/apps/dc48226e64/releases/")
+    local("""curl %s \
+    -H "Authorization: Bearer 2d0f64cf9464a3f383554dce3311e709fd928a5c" \
+    -d rev=`git log -n 1 --pretty=format:%H` \
+    -d branch=`git rev-parse --abbrev-ref HEAD` \
+    -d status=completed""" % url)
+
+
 def deploy():
     code_dir = "/var/www/artsho/artsho"
     with cd(code_dir):
@@ -24,6 +34,7 @@ def deploy():
             run(("rsync -avp media/ "
                  "%s:/var/www/artsho/artsho/media/") % n)
     restart_gunicorn()
+    opbeat()
 
 
 def design_deploy():
