7e1bb9ff0090c0f6a37cb8894d3244e66880bbc2
Anders Pearson <anders@columbia.edu>
2016-09-04 23:42:26 +0100

    also need request context processor

--- artsho/settings_shared.py
+++ artsho/settings_shared.py
@@ -66,6 +66,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'artsho.main.contextprocessors.menu_items',
